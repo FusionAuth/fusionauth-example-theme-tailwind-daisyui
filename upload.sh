34ce323bc0240b129a4fcc15777442e4aefdf430
Colin Frick <colin+github@sonderformat.llc>
2023-06-15 09:39:18 +0200

    remove old project

--- upload.sh
+++
@@ -1,7 +0,0 @@
-. .env
-
-node buildjson.js > $TMP_DIR/updated.json
-
-curl -XPATCH -H 'Content-type: application/json' -H 'Authorization: '$API_KEY $FUSIONAUTH_URL/api/theme/$THEME_ID -d@$TMP_DIR/updated.json > /dev/null
-
-rm $TMP_DIR/updated.json
